fc868e5d6484f911579758b622d16ab0ea8fa16a
Ankush Agarwal <ankushagarwal11@gmail.com>
2022-08-04 16:49:03 -0700

    Remove dist from gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import "@nomiclabs/hardhat-ethers";
+import "./type-extensions";
+import { UniswapV3Deployer } from "./deployer/UniswapV3Deployer";
+export { UniswapV3Deployer };
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UniswapV3Deployer = void 0;
+const config_1 = require("hardhat/config");
+require("@nomiclabs/hardhat-ethers");
+require("./type-extensions");
+const cli_table3_1 = __importDefault(require("cli-table3"));
+const UniswapV3Deployer_1 = require("./deployer/UniswapV3Deployer");
+Object.defineProperty(exports, "UniswapV3Deployer", { enumerable: true, get: function () { return UniswapV3Deployer_1.UniswapV3Deployer; } });
+config_1.task("deploy-uniswap", "Deploys Uniswap V3 contracts", async (args, hre) => {
+    const [actor] = await hre.ethers.getSigners();
+    const contracts = await UniswapV3Deployer_1.UniswapV3Deployer.deploy(actor);
+    const table = new cli_table3_1.default({
+        head: ["Contract", "Address"],
+        style: { border: [] },
+    });
+    for (const item of Object.keys(contracts)) {
+        table.push([item, contracts[item].address]);
+    }
+    console.info(table.toString());
+});
+//# sourceMappingURL=index.js.map
